01108114b8ea8690730bb428258d6342ab1f3a2b
Jason Calderon <caldjaso@sheridancollege.ca>
2021-02-01 22:48:33 -0500

    Second Commit

--- src/main/java/ca/sheridancollege/caldjaso/controllers/GrommetController.java
+++ src/main/java/ca/sheridancollege/caldjaso/controllers/GrommetController.java
@@ -1,7 +1,20 @@
 package ca.sheridancollege.caldjaso.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class GrommetController {
+    @GetMapping("/")
+    public String index(){
+
+        return "index";
+    }
+
+    @PostMapping("/grommetFactory")
+    public String processGrommets(){
+
+        return "DisplayGrommets";
+    }
 }
